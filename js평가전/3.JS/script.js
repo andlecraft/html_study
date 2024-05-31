e78b9f343593b2f7f6e2baf05a98fb8de84004bc
andlecraft <42287993+andlecraft@users.noreply.github.com>
2024-05-31 14:44:28 +0900

    js
    js

--- js평가전/3.JS/script.js
+++ js평가전/3.JS/script.js
@@ -1,11 +1,43 @@
 class Shop{
-    constructor(data){
-        this.data = data
-        console.log(data)
+    constructor(json){
+        this.json = json
+        this.objectList=[]
+        this.objectSortData()
+        this.drawObjectList()
+
+        // console.log(json)
+
+
+    }
+
+    objectSortData(){
+        this.json.forEach(json=>{
+            this.objectList.push(json)
+            console.log(json)
+        })
+    }
+    drawObjectList(){
+        this.objectList.forEach(json=>{
+            $('.objects').append(`
+            <div class="object">
+            <div class="img">
+
+            </div>
+            <div class="texts">
+                <p class="title">${json.product_name}</p>
+                <p class="price">💵${json.price}</p>
+                <div class="bottom">
+                    <p class="date">${json.create_date}</p>
+                <p class="like">${json.like}♡</p>
+                </div>
+            </div>
+        </div>
+            `)
+        })
     }
 
      
 }
-$.getJSON('/data.json', json => {
-    new Shop(json.data)
+$.getJSON('/3.JS/data.json', json => {
+    new Shop(json)
 })
